862dca7aeb29343513565cdfcae8ba946d109933
Amr Abdel-Wahab <amr@orcas.io>
2020-02-24 20:06:36 +0200

    Initial Commit

---
+++ Arabic Voice Recognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arabic Voice Recognition
+//
+//  Created by Amr AbdelWahab on 2/24/20.
+//  Copyright © 2020 Orcas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
